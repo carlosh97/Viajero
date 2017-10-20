57826bb07f20aaa1009727083b416d5150044c94
Carlos <carlosh97@live.com>
2017-10-19 22:54:12 -0500

    Initial Commit

---
+++ Viajero/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Viajero
+//
+//  Created by Carlos González on 10/19/17.
+//  Copyright © 2017 Carlos González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
